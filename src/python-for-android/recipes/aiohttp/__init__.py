ae2f1944db0092c4862f3370f5ff2aa2fbd59316
Xavier Fiechter <xavierfiechter@gmail.com>
2022-10-19 01:17:26 +0200

    added recipes for aiohttp==3.8.3, take2

--- src/python-for-android/recipes/aiohttp/__init__.py
+++ src/python-for-android/recipes/aiohttp/__init__.py
@@ -1,6 +1,9 @@
-class AIOHTTPRecipe(CythonRecipe):  # type: ignore # pylint: disable=R0903
-    """Build AIOHTTP"""
+from pythonforandroid.recipe import PythonRecipe # CythonRecipe
 
+
+class AIOHTTPRecipe(PythonRecipe):
     version = "v3.8.3"
     url = "https://github.com/aio-libs/aiohttp/archive/{version}.zip"
     name = "aiohttp"
+
+recipe = AIOHTTPRecipe()
