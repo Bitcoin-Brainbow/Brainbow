797c2be030fd3791b3221effe2b34c6483d22dad
Xavier Fiechter <xavierfiechter@gmail.com>
2022-10-19 01:58:56 +0200

    added recipes for aiohttp==3.8.3, take4

--- src/python-for-android/recipes/aiohttp/__init__.py
+++ src/python-for-android/recipes/aiohttp/__init__.py
@@ -1,10 +1,18 @@
 from pythonforandroid.recipe import PythonRecipe # CythonRecipe
 
 
-class AIOHTTPRecipe(PythonRecipe):
+#class AIOHTTPRecipe(PythonRecipe):
+from pythonforandroid.recipe import IncludedFilesBehaviour, CppCompiledComponentsPythonRecipe
+import os
+import sys
+
+class AIOHTTPRecipe(IncludedFilesBehaviour, CppCompiledComponentsPythonRecipe):
+
     version = "v3.8.3"
     url = "https://github.com/aio-libs/aiohttp/archive/{version}.zip"
     name = "aiohttp"
     depends = ['setuptools']
-    
+    call_hostpython_via_targetpython = False
+    install_in_hostpython = True
+
 recipe = AIOHTTPRecipe()
