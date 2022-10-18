602ad6c3e8cabfe69a7d836314a6d481ba930413
Xavier Fiechter <xavierfiechter@gmail.com>
2022-10-19 01:29:15 +0200

    added recipes for aiohttp==3.8.3, take2

--- src/python-for-android/recipes/aiohttp/__init__.py
+++ src/python-for-android/recipes/aiohttp/__init__.py
@@ -5,5 +5,6 @@ class AIOHTTPRecipe(PythonRecipe):
     version = "v3.8.3"
     url = "https://github.com/aio-libs/aiohttp/archive/{version}.zip"
     name = "aiohttp"
-
+    depends = ['setuptools']
+    
 recipe = AIOHTTPRecipe()
