7b3fd98822f7a3ff6c33e942e2cd8ddab60d80e8
Xavier Fiechter <xavierfiechter@gmail.com>
2022-10-20 13:05:35 +0200

    aiohttp

--- src/python-for-android/recipes/aiohttp/__init__.py
+++ src/python-for-android/recipes/aiohttp/__init__.py
@@ -1,16 +1,13 @@
+"""Build AIOHTTP"""
+from typing import List
 from pythonforandroid.recipe import CppCompiledComponentsPythonRecipe
 
-class AIOHTTPRecipe(CppCompiledComponentsPythonRecipe):
-    """
-    See:
-    https://stackoverflow.com/a/64755338/20124004,
-    https://stackoverflow.com/a/64755052/20124004 and
-    """
-    # version = "v3.8.3"
-    # url = "https://github.com/aio-libs/aiohttp/archive/{version}.zip"
-    url = "https://files.pythonhosted.org/packages/ff/4f/62d9859b7d4e6dc32feda67815c5f5ab4421e6909e48cbc970b6a40d60b7/aiohttp-3.8.3.tar.gz"
+
+class AIOHTTPRecipe(CppCompiledComponentsPythonRecipe):  # type: ignore # pylint: disable=R0903
+    version = "3.8.3"
+    url = "https://pypi.python.org/packages/source/a/aiohttp/aiohttp-{version}.tar.gz"
     name = "aiohttp"
-    depends = ['setuptools']
+    depends: List[str] = ["setuptools"]
     call_hostpython_via_targetpython = False
     install_in_hostpython = True
 
@@ -18,5 +15,6 @@ class AIOHTTPRecipe(CppCompiledComponentsPythonRecipe):
         env = super().get_recipe_env(arch)
         env['LDFLAGS'] += ' -lc++_shared'
         return env
+
 
 recipe = AIOHTTPRecipe()
